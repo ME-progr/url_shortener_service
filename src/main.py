0a0e6c333fa34f09b1ee56f5ff2591ad158d1e9f
E.Moiseev <sk831it3@mail.ru>
2023-06-13 09:25:33 +0300

    add docker compose, launch with redis

--- src/main.py
+++ src/main.py
@@ -1,17 +1,34 @@
-"""Модуль, запускающий FastApi-приложение (`uvicorn` сервер)."""
+"""Модуль, запускающий FastApi-приложение."""
+
+from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+from redis.asyncio.client import Redis
 
 from src.core.config import settings
 
+description = """
+### API для перенаправления на длинные ссылки.<br>
+"""
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    redis_connection = await Redis(host=settings.redis_host, port=settings.redis_port)
+    await redis_connection.set('test_key', '11111111111111')
+    yield
+
+    await redis_connection.close()
 
 app = FastAPI(
     title=settings.project_name,
+    description=description,
     docs_url='/api/openapi',
     openapi_url='/api/openapi.json',
     default_response_class=ORJSONResponse,
+    lifespan=lifespan,
 )
 
 
